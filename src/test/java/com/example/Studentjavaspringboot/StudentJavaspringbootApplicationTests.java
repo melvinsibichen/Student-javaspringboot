c4d9dcb4ed42fdbfd76aae5b89279f9207d84cb9
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-09 01:36:53 +0530

    propcrtd

---
+++ src/test/java/com/example/Studentjavaspringboot/StudentJavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Studentjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentJavaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
