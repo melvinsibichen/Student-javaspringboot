96a063f39efe5f5b33d18442d5a5d3bbab2422ee
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-10 17:58:18 +0530

    fixed

--- src/main/java/com/example/studentappnew_backend/StudentJavaspringbootApplication.java
+++ src/main/java/com/example/studentappnew_backend/StudentJavaspringbootApplication.java
@@ -1,4 +1,4 @@
-package com.example.Studentjavaspringboot;
+package com.example.studentappnew_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/studentappnew_backend/StudentJavaspringbootApplicationTests.java
+++ src/test/java/com/example/studentappnew_backend/StudentJavaspringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Studentjavaspringboot;
+package com.example.studentappnew_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
